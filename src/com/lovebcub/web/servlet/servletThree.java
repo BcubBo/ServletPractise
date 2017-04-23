38032c3334cefb0b3dfaf4b5b0698cce38b7adca
BcubBo <fatalitywolf@foxmail.com>
2017-04-23 16:42:52 +0800

    小幅度更改错误
    Change-Id: I7a6487e83a2d9fc6a80e7009c60755dcdb2f92f3
    Change-Id: 00002
    Signed-off-by: BcubBo <fatalitywolf@foxmail.com>

--- src/com/lovebcub/web/servlet/servletThree.java
+++ src/com/lovebcub/web/servlet/servletThree.java
@@ -10,30 +10,30 @@ import javax.servlet.http.HttpServletResponse;
 
 public class servletThree extends HttpServlet {
 
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println(">>>>>>>>doGet()方法被调用");
-		super.doGet(req, resp);
-	}
-
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println(">>>>>>>>doPost()方法被调用");
-		super.doPost(req, resp);
-	}
+	
 
 	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
-		System.out.println(">>>>>>>>>service()方法，被调用");
+		System.out.println(">>>>>>>>>service()方法调度器进行相应的post或者是get请求的调度，被调用");
 		super.service(arg0, arg1);
+		//派发器里的super不可取消
 		//派发器
 	}
 
 	public void destroy() {
-		super.destroy();
+		System.out.println(">>>>>>>>>>>>destroy()方法被调用");
 	}
 
 	public void init(ServletConfig config) throws ServletException {
 		System.out.println(">>>>>>>>>initialize the Servlet初始化开始");
-		super.init(config);
+		System.out.println(">>>>>>>>取得初始化配置设置的参数setCharContent的值:"+config.getInitParameter("charSetContent"));
 		
+	}
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println(">>>>>>>>doGet()方法被调用");
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println(">>>>>>>>doPost()方法被调用");
 	}
 
 }
